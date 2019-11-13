bef265ee902ef6516a86fb8a36342152298f3618
Antony Sanders <antony.devon92@gmail.com>
2019-11-13 22:51:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
 
-end
+
+if array.length == 1
+  return array.join
+elsif array.length == 2 
+  return array.join(" and ")
+else 
+  last = "and " + array.pop
+  array << last
+  array.join(", ")
+
+
+end 
+  
+
+end 
