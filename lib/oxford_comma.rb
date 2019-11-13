10c6695996248d22db6455880b5c513267dbfac1
Antony Sanders <antony.devon92@gmail.com>
2019-11-13 22:26:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,22 +1,24 @@
 require "pry"
 
 def oxford_comma(array)
- binding.pry
- 
-#array = "kiwi"
+
 
 if array.length == 1
   return array.join
 elsif array.length == 2 
   return array.join(" and ")
-elsif array.length == 3
-  array.join(",")
+else 
+  array.join(", ")
+  last = array.pop
   
+   binding.pry
   return 
   #return "kiwi, durian, and starfruit"
-else array.length > 3
-  return array.join (", ")
-end 
+
+
+#else array.length > 3
+#  return array.join (", ")
+#end 
   
 
 end 
